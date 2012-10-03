33bcb4649f46c43a5a0f18aa9a9c2eed6dadc6ef
Stephen Neyens <neyens.s@gmail.com>
2012-10-03 09:49:20 -0400

    Init commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebAddressBook::Application.config.secret_token = '7d965772bb8e71fd74984740c1f1d07dcf877b68870d0198570a0e2c42cdd30ee69a7574bec1a4ee878b192f0236c39db03dcc61c3a7a6f98c866b1beb17bc69'
